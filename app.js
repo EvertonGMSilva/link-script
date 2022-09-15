fbddc09ff2a9eb9451c63e5e105402ea74506088
Gabriel Stamato <stamato7@gmail.com>
2022-09-15 10:02:18 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const fs = require("fs");
+const ImageLinks = require("./utils/image_links.json");
+
+(async () => {
+  let data = [];
+  let count = 0
+
+  async function getURL(element) {
+    console.log(`testando url: ${element.Source}`);
+
+    try {
+      const response = await axios.get(element.Source);
+      if (response.status !== 200) {
+        data.push({
+          src: element.Source,
+          destination: element.Destination,
+          status: response.status,
+        });
+      }
+      console.log(
+        `finalizando teste url: ${element.Source}, status: ${response.status}`
+      );
+    } catch (error) {
+      data.push(JSON.stringify({
+        src: element.Source,
+        destination: element.Destination,
+        status: error.response.status,
+      }));
+      console.log(
+        `finalizando teste url: ${element.Source}, status: ${error.response.status}`
+      );
+    }
+  }
+
+  for (element of ImageLinks) {
+    count = count + 1
+    await getURL(element);
+    
+    if(data.length === 50){
+    fs.writeFile(`arquivo${new Date().toISOString()}.json`, data.toString(), (err) => {
+      if (err) throw err;
+      console.log("O arquivo foi criado!");
+    });
+    fs.writeFile(`urlsconsultadas.txt`, count.toString(), (err) => {
+      if (err) throw err;
+      console.log(`Foram consultadas ${count} urls`);
+    });
+    data = []
+  }
+  }
+
+})();
